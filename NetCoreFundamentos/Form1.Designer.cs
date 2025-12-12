97bb6045c75a851c9095bdf6b068dd50cd6a0e29
Alejandro4092 <acobomarcos@gmail.com>
2025-12-12 10:20:43 +0100

    viernes

--- NetCoreFundamentos/Form1.Designer.cs
+++ NetCoreFundamentos/Form1.Designer.cs
@@ -28,18 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            txtNombre = new TextBox();
+            btnPulsar = new Button();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(420, 160);
+            label1.Name = "label1";
+            label1.Size = new Size(123, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Introduzca su nombre";
+            label1.Click += label1_Click;
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(405, 226);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(172, 23);
+            txtNombre.TabIndex = 2;
+            // 
+            // btnPulsar
+            // 
+            btnPulsar.Location = new Point(405, 285);
+            btnPulsar.Name = "btnPulsar";
+            btnPulsar.Size = new Size(172, 51);
+            btnPulsar.TabIndex = 3;
+            btnPulsar.Text = "Pulsar";
+            btnPulsar.UseVisualStyleBackColor = true;
+            btnPulsar.Click += btnPulsar_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(980, 629);
+            BackColor = Color.Cyan;
+            ClientSize = new Size(892, 565);
+            Controls.Add(btnPulsar);
+            Controls.Add(txtNombre);
+            Controls.Add(label1);
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+        private Label label1;
+        private TextBox txtNombre;
+        private Button btnPulsar;
     }
 }
--- NetCoreFundamentos/Form1.cs
+++ NetCoreFundamentos/Form1.cs
@@ -6,5 +6,27 @@ namespace NetCoreFundamentos
         {
             InitializeComponent();
         }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPulsar_Click(object sender, EventArgs e)
+        {
+            //solo escribir aqui
+            this.txtNombre.Location = new Point(10,30);
+            this.txtNombre.Text = "Soy un texto nuevo!!!";
+            this.txtNombre.Width = 220;
+            this.txtNombre.BackColor = Color.Fuchsia;
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
