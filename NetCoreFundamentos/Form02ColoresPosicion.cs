6d54014bfc993b859abba0a515fb1e88a243e46b
Alejandro4092 <acobomarcos@gmail.com>
2025-12-15 09:43:19 +0100

    ifs

--- NetCoreFundamentos/Form02ColoresPosicion.cs
+++ NetCoreFundamentos/Form02ColoresPosicion.cs
@@ -38,7 +38,23 @@ namespace NetCoreFundamentos
             int rojo = int.Parse(this.txtRojo.Text);
             int verde = int.Parse(this.txtVerde.Text);
             int azul = int.Parse(this.txtAzul.Text);
-            this.BackColor = Color.FromArgb(rojo, verde, azul);
-        } 
+            if (rojo < 0 || rojo > 255)
+            {
+                MessageBox.Show("El valor de rojo debe estar comprendido entre 0 y 255");
+
+            }
+            else if (verde < 0 || verde > 255)
+            {
+                MessageBox.Show("El valor de verde debe estar comprendido entre 0 y 255");
+            }
+            else if (azul < 0 || azul > 255)
+            {
+                MessageBox.Show("El valor de azul debe estar comprendido entre 0 y 255");
+            }
+            else
+            {
+                this.BackColor = Color.FromArgb(rojo, verde, azul);
+            }
+            } 
     }
 }
