e6e10bc69f3d55d0dcc34aca008c1fbed31bf766
Alejandro4092 <acobomarcos@gmail.com>
2025-12-15 10:15:13 +0100

    Dias nacimiento

---
+++ NetCoreFundamentos/Form03DiaNacimiento.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace NetCoreFundamentos
+{
+    partial class Form03DiaNacimiento
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnNacimiento = new Button();
+            txtDia = new TextBox();
+            txtMes = new TextBox();
+            txtAño = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            lblDia = new Label();
+            SuspendLayout();
+            // 
+            // btnNacimiento
+            // 
+            btnNacimiento.Location = new Point(114, 248);
+            btnNacimiento.Name = "btnNacimiento";
+            btnNacimiento.Size = new Size(100, 49);
+            btnNacimiento.TabIndex = 0;
+            btnNacimiento.Text = "Mostrar Dia";
+            btnNacimiento.UseVisualStyleBackColor = true;
+            btnNacimiento.Click += btnNacimiento_Click;
+            // 
+            // txtDia
+            // 
+            txtDia.Location = new Point(114, 45);
+            txtDia.Name = "txtDia";
+            txtDia.Size = new Size(100, 23);
+            txtDia.TabIndex = 1;
+            txtDia.TextChanged += txtDia_TextChanged;
+            // 
+            // txtMes
+            // 
+            txtMes.Location = new Point(114, 110);
+            txtMes.Name = "txtMes";
+            txtMes.Size = new Size(100, 23);
+            txtMes.TabIndex = 2;
+            txtMes.TextChanged += txtMes_TextChanged;
+            // 
+            // txtAño
+            // 
+            txtAño.Location = new Point(114, 162);
+            txtAño.Name = "txtAño";
+            txtAño.Size = new Size(100, 23);
+            txtAño.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(49, 50);
+            label1.Name = "label1";
+            label1.Size = new Size(24, 15);
+            label1.TabIndex = 4;
+            label1.Text = "Dia";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(49, 113);
+            label2.Name = "label2";
+            label2.Size = new Size(29, 15);
+            label2.TabIndex = 5;
+            label2.Text = "Mes";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(54, 169);
+            label3.Name = "label3";
+            label3.Size = new Size(29, 15);
+            label3.TabIndex = 6;
+            label3.Text = "Año";
+            // 
+            // lblDia
+            // 
+            lblDia.AutoSize = true;
+            lblDia.Location = new Point(100, 332);
+            lblDia.Name = "lblDia";
+            lblDia.Size = new Size(0, 15);
+            lblDia.TabIndex = 7;
+            // 
+            // Form03DiaNacimiento
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblDia);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtAño);
+            Controls.Add(txtMes);
+            Controls.Add(txtDia);
+            Controls.Add(btnNacimiento);
+            Name = "Form03DiaNacimiento";
+            Text = "Form03DiaNacimiento";
+            Load += Form03DiaNacimiento_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnNacimiento;
+        private TextBox txtDia;
+        private TextBox txtMes;
+        private TextBox txtAño;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label lblDia;
+    }
+}
---
+++ NetCoreFundamentos/Form03DiaNacimiento.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NetCoreFundamentos
+{
+    public partial class Form03DiaNacimiento : Form
+    {
+        public Form03DiaNacimiento()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form03DiaNacimiento_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtDia_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtMes_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNacimiento_Click(object sender, EventArgs e)
+        {
+            int dia= int.Parse(this.txtDia.Text);
+            int mes = int.Parse(this.txtMes.Text);
+            int anyo = int.Parse(this.txtAño.Text);
+            if (mes == 1)
+            {
+                mes = 13;
+                anyo--;
+
+            }
+            if (mes == 2)
+            {
+                mes = 14;
+                anyo--;
+            }
+            int mes2 = ((mes + 1) * 3) / 5;
+            int anyo2 = anyo / 4;
+            int anyo3 = anyo / 100;
+            int anyo4 = anyo / 400;
+            int operacion1 = dia + (mes * 2) + anyo + mes2 + anyo2 - anyo3 + anyo4 + 2;
+            int resultado = operacion1 / 7;
+            int resultadodefinitivo = operacion1 - (resultado * 7);
+
+            switch (resultadodefinitivo)
+            {
+                case 0:
+                    lblDia.Text = "Tu dia es sabado";
+                    break;
+                case 1:
+                    lblDia.Text = "Tu dia es Domingo";
+                    break;
+                case 2:
+                    lblDia.Text = "Tu dia es Lunes";
+                    break;
+                case 3:
+                    lblDia.Text = "Tu dia es Martes";
+                    break;
+                case 4:
+                    lblDia.Text = "Tu dia es Miercoles";
+                    break;
+                case 5:
+                    lblDia.Text = "Tu dia es Jueves";
+                    break;
+                case 6:
+                    lblDia.Text = "Tu dia es Viernes";
+                    break;
+
+            }
+        }
+    }
+}
